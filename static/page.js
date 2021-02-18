9ad7d768ee5e314be7ecc64fdc5fe2bfa61e688c
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-02-18 18:43:03 -0500

    Update page.js

--- static/page.js
+++ static/page.js
@@ -65,39 +65,7 @@ module.exports = function (req, res, url) {
 			};
 			break;
 		}
-			
-                        case "/cc": {
-			title = "Character Creator";
-			attrs = {
-				data: process.env.SWF_URL + "/cc1.swf", // data: 'cc.swf',
-				type: "application/x-shockwave-flash",
-				id: "char_creator",
-				width: "100%",
-				height: "100%",
-			};
-			params = {
-				flashvars: {
-					apiserver: "/",
-					storePath: process.env.STORE_URL + "/<store>",
-					clientThemePath: process.env.CLIENT_URL + "/<client_theme>",
-					original_asset_id: query["id"] || null,
-					themeId: "business",
-					ut: 60,
-					bs: "default",
-					appCode: "go",
-					page: "",
-					siteId: "go",
-					m_mode: "school",
-					isLogin: "Y",
-					isEmbed: 1,
-					ctc: "go",
-					tlang: "en_US",
-				},
-				allowScriptAccess: "always",
-				movie: process.env.SWF_URL + "/cc1.swf", // 'http://localhost/cc.swf'
-			};
-			break;
-		}
+		
 		case "/character/browser": {
 			title = "CC Browser";
 			attrs = {
@@ -168,26 +136,6 @@ module.exports = function (req, res, url) {
 				},
 				allowScriptAccess: "always",
 			};
-			break;
-		}
-			case '/go_full': {
-			let presave = query.movieId && query.movieId.startsWith('m') ? query.movieId :
-				`m-${fUtil[query.noAutosave ? 'getNextFileId' : 'fillNextFileId']('movie-', '.xml')}`;
-			title = 'Video Editor';
-			attrs = {
-				data: process.env.SWF_URL + '/go_full1.swf',
-				type: 'application/x-shockwave-flash', width: '100%', height: '100%',
-			};
-			params = {
-				flashvars: {
-					'apiserver': '/', 'storePath': process.env.STORE_URL + '/<store>', 'isEmbed': 1, 'ctc': 'go',
-					'ut': 60, 'bs': 'default', 'appCode': 'go', 'page': '', 'siteId': 'go', 'lid': 13, 'isLogin': 'Y', 'retut': 1,
-					'clientThemePath': process.env.CLIENT_URL + '/<client_theme>', 'themeId': 'business', 'tlang': 'en_US',
-					'presaveId': presave, 'goteam_draft_only': 1, 'isWide': 1, 'nextUrl': '/html/list.html',
-				},
-				allowScriptAccess: 'always',
-			};
-			sessions.set({ movieId: presave }, req);
 			break;
 		}
 
